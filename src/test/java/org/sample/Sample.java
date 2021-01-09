6b45b7464489d62011408c6308f19420af0db751
Siaarasaudaiyan <siadazzling16@gmail.com>
2021-01-09 12:28:54 +0530

    selenium

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -1,5 +1,10 @@
 package org.sample;
 
-public class Sample {
 
+
+public class Sample   {
+	public static void main(String[] args)  {
+	 System.out.println("java");
+	 System.out.println("selenium");
+}
 }
