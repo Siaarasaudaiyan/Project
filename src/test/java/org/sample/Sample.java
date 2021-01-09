c2dfdb3ee81da5a71d5c6a6319b3c55b56e3a389
Siaarasaudaiyan <siadazzling16@gmail.com>
2021-01-09 14:44:18 +0530

    Second

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -6,5 +6,10 @@ public class Sample   {
 	public static void main(String[] args)  {
 	 System.out.println("java");
 	 System.out.println("selenium");
+	 int a=10;
+	 int b=20;
+	int  c=a+b;
+	System.out.println(c);
+	 
 }
 }
